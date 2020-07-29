94670751a06ac4096be951c49996218ad3ccd799
Jake Bottrall <jakebottrall@gmail.com>
2020-07-29 12:42:19 -0700

    simplify routes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const contact = require("./routes/contact");
-app.use("/api/contact", contact);
+app.use("/ghost-contact", contact);
 
 // error handler
 const errorHandler = require("./handlers/error");
