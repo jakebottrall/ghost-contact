c73a81a6bf7db77a7157210d03d962b03b047235
jake <jake@daciatech.com>
2020-10-12 16:38:29 -0700

    refactor message template and add comments

--- handlers/contact.js
+++ handlers/contact.js
@@ -1,23 +1,28 @@
 const fs = require("fs");
-const verifyRecaptcha = require("../helpers/reCaptcha");
 const path = require("path");
-const Handlebars = require("handlebars");
 const mailgun = require("mailgun-js");
+const Handlebars = require("handlebars");
+const verifyRecaptcha = require("../services/reCaptcha");
 
 exports.contact = async function (req, res, next) {
   try {
-    let verify = await verifyRecaptcha(req.body.reCaptcha);
+    // check google recaptcha token for suspicious activity
+    const verify = await verifyRecaptcha(req.body.reCaptcha);
+
     if (verify.data.success === true) {
+      // grab relevant .env data
       const API_KEY = process.env.MAILGUN_API_KEY;
       const DOMAIN = process.env[req.body.mailgunDomain];
       const mg = mailgun({ apiKey: API_KEY, domain: DOMAIN });
 
+      // import html email template
       const source = fs.readFileSync(
         path.join(__dirname, `../mail_templates/message.hbs`),
         "utf8"
       );
       const template = Handlebars.compile(source);
 
+      // accumulate data from message and env for template
       const templateData = {
         name: req.body.name,
         email: req.body.email,
@@ -26,6 +31,7 @@ exports.contact = async function (req, res, next) {
         logoUrl: req.body.logoUrl,
       };
 
+      // create email
       const data = {
         from: `${req.body.name} <${req.body.email}>`,
         to: req.body.recipient,
@@ -33,6 +39,7 @@ exports.contact = async function (req, res, next) {
         html: template(templateData),
       };
 
+      // send email to mailgun
       await mg.messages().send(data);
 
       return res.status(200).json({ message: "success" });
